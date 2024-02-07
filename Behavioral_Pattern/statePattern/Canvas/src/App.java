58261a24c4a63f7b4ebf2b3ceb927a68a10fbea2
Mayesha <mim2007088@stud.kuet.ac.bd>
2024-02-07 23:24:31 +0600

    added state pattern

---
+++ Behavioral_Pattern/statePattern/Canvas/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        var canvas = new Canvas();
+        canvas.setCurrentTool(new SelectionTool());
+        canvas.mouseDown();
+        canvas.mouseUp();
+    }
+}
